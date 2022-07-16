6478aa340ed1ec8144dfb899141e78653b98b3bc
andikaputri2310 <105660235+andikaputri2310@users.noreply.github.com>
2022-07-16 11:09:10 +0700

    Add files via upload

---
+++ app.R
@@ -0,0 +1,21 @@
+library(shiny)
+
+#import image harus di folder www
+# Define UI ----
+ui <- fluidPage(
+  titlePanel("My Shiny App"),
+  sidebarLayout(
+    sidebarPanel(),
+    mainPanel(
+      img(src = "build-web-apps-tutorial.png", height = 140, width = 400)
+    )
+  )
+)
+
+# Define server logic ----
+server <- function(input, output) {
+  
+}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
